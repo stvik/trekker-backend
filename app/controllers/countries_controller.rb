aa1f4681046648b7b3f41a658286bef083eb38fb
stvik <stvik2@gmail.com>
2020-01-29 11:59:45 -0500

    fixed serializer

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,4 +5,12 @@ class CountriesController < ApplicationController
 		countries = Country.all
 		render json: CountrySerializer.new(countries).to_serialized_json
 	end
+
+	def show
+		country = Country.find(params[:id])
+		render json: CountrySerializer.new(country).to_serialized_json_with_cities
+	end
+
+
+
 end
--- app/services/country_serializer.rb
+++ app/services/country_serializer.rb
@@ -1,12 +1,18 @@
 class CountrySerializer
 
-def initialize(country_object)
-	@country = country_object
-end
+	def initialize(country_object)
+		@country = country_object
+	end
 
-def to_serialized_json
-	@country.to_json( :except => [:created_at, :updated_at])
+	def to_serialized_json
+		@country.to_json( :except => [:created_at, :updated_at])
 
-end
+	end
+
+	def to_serialized_json_with_cities
+		@country.to_json( :include => {
+			:cities => {:except => [:country_id, :created_at, :updated_at]}
+		}, :except => [:updated_at, :created_at])
+	end
 
 end
