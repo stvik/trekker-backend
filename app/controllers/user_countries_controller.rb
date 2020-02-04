044f78edd34a8bfc71d594ca301e218b93243333
stvik <stvik2@gmail.com>
2020-02-04 10:31:47 -0500

    can create user_country

--- app/controllers/user_countries_controller.rb
+++ app/controllers/user_countries_controller.rb
@@ -1,2 +1,21 @@
 class UserCountriesController < ApplicationController
+
+	def index
+		render json: UserCountry.all
+	end
+
+	def create
+		uc = UserCountry.create(user_country_params)
+
+		render json: uc
+	end
+
+	private
+
+	def user_country_params
+		params.require(:user_country).permit(:user_id, :country_id, :visited, :travel_goal, :recommendation)
+
+	end
+
+
 end
--- app/services/user_serializer.rb
+++ app/services/user_serializer.rb
@@ -5,7 +5,14 @@ class UserSerializer
 	end
 
 	def to_serialized_json
-		@user.to_json( :except => [:password, :created_at, :updated_at])
+		@user.to_json( 
+			:include => {
+			:user_countries => {
+				:include => {
+					:country => {:only => [:name, :latitude, :longitude]}},
+				:except => [:created_at, :updated_at, :recommendation, :user_id]}
+			},
+			:except => [:password, :created_at, :updated_at])
 	end
 
 end
